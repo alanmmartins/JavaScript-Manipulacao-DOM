53d156b22a73ee6e47175f7e748f71219ba4ed7c
Alan Munoz Martins <alanmunozmartins@gmail.com>
2023-07-31 19:43:50 -0300

    script texto passo a passo orientacao

---
+++ script.js
@@ -0,0 +1,22 @@
+/* Algoritmo:
+
+1. Pegar os valores
+2. Calcular a Idade
+      a. Com base no ano
+      b. Com mês (EXTRA)
+      c. Com dia (EXTRA)
+
+3. Gerar a faixa etária
+   
+    Resultado            Faixa
+    0 à 12                Criança
+    13 à 17                Adolescente
+    18 à 65               Adulto
+    Acima de 65         Idoso
+   
+
+4. Organizar o objeto pessoa para salvar na lista
+5. Cadastrar a pessoa na lista
+6. Função para carregar as pessoas, carrega a lista do localStorage, chamar ao carregar a página
+7. Renderizar o conteúdo da tabela com as pessoas cadastradas
+8. Botão para limpar os registros; */
