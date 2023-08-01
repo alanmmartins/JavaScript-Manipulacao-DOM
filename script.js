e491b8726b74be4e1ebf7da8e9a4db6f5667188f
alanmmartins <alanmunozmartins@gmail.com>
2023-08-01 14:07:44 -0300

    classificando a idade

--- script.js
+++ script.js
@@ -30,6 +30,11 @@ function calculaIdade(event) {
     let dadosUsuario = pegarValores();
     let idade = calcular(dadosUsuario.ano)
 
+    let classificacaoIdade = classificarIdade(idade);
+
+    let dadosUsuarioAtualizado = organizarDados(dadosUsuario, idade, classificacaoIdade);
+    cadastrarUsuario(dadosUsuarioAtualizado);
+
     /*  console.log("funciono!!!!!!!!!!!!"); */
 }
 
@@ -82,3 +87,124 @@ function calcular(ano, mes, dia) {
     18 à 65               Adulto
     Acima de 65         Idoso
     */
+    function classificarIdade(idade) {
+        if (idade >= 0 && idade <= 12) {
+            return 'Criança';
+          } else if (idade >= 13 && idade <= 17) {
+            return 'Adolescente';
+          } else if (idade >= 18 && idade <= 65) {
+            return 'Adulto';
+          } else if (idade > 65) {
+            return 'Idoso';
+          } /* else {
+            return 'idade A.C';
+          } */
+        
+    }
+    
+
+    
+    //Passo 4 - Organizar Informacoes 
+    function organizarDados(dadosUsuario, valoridade, classificaridade){
+    
+        let idadeAtualizado = {
+            ...dadosUsuario,
+            idade: valoridade,
+            classificacao: classificaridade,
+        }
+    
+        console.log(idadeAtualizado);
+    
+        return idadeAtualizado;
+    }
+    
+    //Passo 5 - Salvar
+    function cadastrarUsuario(usuario) {
+        // Cria um array vazio
+        let listaUsuarios = [];
+    
+        // Verifica se existe o item "usuariosCadastrados" no localStorage
+        //if (localStorage.getItem("usuariosCadastrados") == true) {
+        if (localStorage.getItem("usuariosCadastrados")) {
+            // Caso o item exista ele converte e salva no array criado
+            listaUsuarios = JSON.parse(localStorage.getItem("usuariosCadastrados"));
+        }
+    
+        // Acrescenta o usuario recebido a lista
+        listaUsuarios.push(usuario)
+    
+        // Salva o item "usuariosCadastrados" no localStorage com o conteudo do array convertido para string
+        localStorage.setItem("usuariosCadastrados", JSON.stringify(listaUsuarios))
+    }
+    
+    
+    // Passo 6 - Ler lista
+    function carregarUsuarios() {
+        // Cria um array vazio
+        let listaUsuarios = [];
+    
+        // Verifica se existe o item "usuariosCadastrados" no localStorage
+        if (localStorage.getItem("usuariosCadastrados")) {
+            // Caso o item exista ele converte e salva no array criado
+            listaUsuarios = JSON.parse(localStorage.getItem("usuariosCadastrados"));
+        }
+    
+        // Verifica se o tamanho do array é igual a zero
+        if (listaUsuarios.length == 0) {
+            // Se o tamanho do array for igual a zero,
+            // Obtém o elemento html (tabela) pelo ID
+            let tabela = document.getElementById("corpo-tabela");
+    
+            //Cria uma linha na tabela com a mensagem "Nenhum usuario cadastrado!""
+            tabela.innerHTML = `<tr class="linha-mensagem">
+            <td colspan="6">Nenhum usuario cadastrado!</td>
+        </tr>`
+    
+        }else{
+            // Se o tamanho do array for diferente de zero, ou seja tem algum item dentro do array
+            // Executa a função montarTabela
+            montarTabela(listaUsuarios);
+        }
+    }
+    
+    // Adiciona o evento a janela/navegador que executa a função carregarUsuarios quando o DOM estiver carregado
+    window.addEventListener('DOMContentLoaded', () => carregarUsuarios());
+    
+    
+    // Passo 7 - Montar tabela
+    function montarTabela(listaDeCadastrados) {
+        // Obtém o elemento html (tabela) pelo ID
+        let tabela = document.getElementById("corpo-tabela");
+    
+        // Cria uma variavel vazia
+        let template = '';
+    
+        console.log(listaDeCadastrados);
+    
+        // Obtém cada item dentro do array listaDeCadastrados
+        listaDeCadastrados.forEach(pessoa => {
+            // Cria uma linha de tabela mesclando tag html e valor dos atributos do objeto que esta dentro do array
+            // E adiciona um bloco de codigo igual o a baixo dentro da variavel template para cada elemento do array
+            template += `<tr>
+            <td data-cell="nome">${pessoa.nome}</td>
+            <td data-cell="data de nascimento">${pessoa.dia}-${pessoa.mes}-${pessoa.ano}</td>
+            <td data-cell="idade">${pessoa.idade}</td>
+            <td data-cell="faixa etária">${pessoa.classificacao}</td>
+        </tr>`
+     
+        });
+    
+        // Adiciona o conteudo que esta dentro da variavel template ao elemento tabela
+        tabela.innerHTML = template;
+    }
+    
+    
+    // Passo 8 - Limpar local storage
+    function deletarRegistros() {
+        // Remove o item "usuariosCadastrados" do localStorage
+        localStorage.removeItem("usuariosCadastrados")
+    
+        // Recarrega a pagina
+        window.location.reload();
+    
+    }
