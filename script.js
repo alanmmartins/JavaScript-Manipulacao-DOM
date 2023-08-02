e6269bfddafabecfeab15cf48a0f9c353053f447
Alan Munoz Martins <alanmunozmartins@gmail.com>
2023-08-01 21:51:59 -0300

    comentario para explicar

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
     18 à 65               Adulto
     Acima de 65         Idoso
    
-
+//================================================================
 4. Organizar o objeto pessoa para salvar na lista
 5. Cadastrar a pessoa na lista
 6. Função para carregar as pessoas, carrega a lista do localStorage, chamar ao carregar a página
